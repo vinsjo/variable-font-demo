b545cd177a2f3a2ad976a3799d7596dea9fc72aa
VinSjo <vincentsjogren@gmail.com>
2022-02-11 15:19:21 +0100

    testing gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/variable-font-demo/',
 	plugins: [react()],
 	build: {
 		target: 'es2015',
