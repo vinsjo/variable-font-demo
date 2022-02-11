d5b58e8fff4ce421aec4309c844ba73a39863917
VinSjo <vincentsjogren@gmail.com>
2022-02-11 15:20:43 +0100

    removed gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/variable-font-demo/',
 	plugins: [react()],
 	build: {
 		target: 'es2015',
